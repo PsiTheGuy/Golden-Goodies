c59f7f5551145266acec8321208269cc13ce242d
PsiTheGuy <bobdoor.nm@gmail.com>
2024-05-04 14:25:16 -0400

    Initial commit

---
+++ src/main/java/net/psitheguy/goldengoodies/GoldenGoodies.java
@@ -0,0 +1,16 @@
+package net.psitheguy.goldengoodies;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GoldenGoodies implements ModInitializer {
+	public static final String MOD_ID = "psis-golden-goodies";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/psitheguy/goldengoodies/GoldenGoodiesClient.java
@@ -0,0 +1,10 @@
+package net.psitheguy.goldengoodies;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GoldenGoodiesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
